49eb86db9e509960933099b1b374e4f3ad933290
duong91002 <nguyenhaiduong9102@gmail.com>
2025-08-13 03:10:42 +0700

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const { verifyToken } = require("./src/utils/jwt");
+const http = require("http");
+
+const userRoutes = require("./src/routes/user.route");
+const roomRoutes = require("./src/routes/room.route");
+const authRoutes = require("./src/routes/auth.route");
+const chatRoutes = require("./src/routes/chat.route");
+const initSocket = require("./src/socket/index");
+dotenv.config();
+
 const app = express();
+const server = http.createServer(app);
 
-app.get("/", (req, res) => {
-  res.send("hello");
-});
+connectDB();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
+app.use("/api/rooms", roomRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/chats", chatRoutes);
+
+initSocket(server);
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
